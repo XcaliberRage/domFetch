6d0fb3703e6eb02394eef58d6cc8673265efcbe7
XcaliberRage <xcalibershard@gmail.com>
2020-03-14 19:11:06 +0100

    Copied out the example code for basic scraping with Colly (see go-colly.org)

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
-import "fmt"
+// Take a user input in the form of a web address
+
+// Return a list of all assets and web pages
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
 
 func main() {
 
-fmt.Println("Hello world")
+	// Instaniate the collector
+	c := colly.NewCollector(
+		// Limit domains to only these
+		colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
+	)
+
+	// Callback on every href element
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link on page
+		// Only links in Allowed Domains is visited
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Print before making a Request
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.Visit("https://hackerspaces.org/")
 
 }
